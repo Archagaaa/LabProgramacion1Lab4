d2f1b8da09de16133c66eae9f64d99ccb0da9476
JonatanRamos919 <ramosjona919@gmail.com>
2025-05-16 10:25:40 -0600

    ejercicio 2 terminado

--- src/labprogramacion1semana4/ramos_jonatan__filtrar.java
+++ src/labprogramacion1semana4/ramos_jonatan__filtrar.java
@@ -3,12 +3,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package labprogramacion1semana4;
-
+import java.util.Scanner;
 /**
  *
  * @author Jona Ramos
  */
 public class ramos_jonatan__filtrar {
-    mai Object object  = new Object();
+    public static void main(String[]args){
+   
+       Scanner lea=new Scanner(System.in);
+       lea.useDelimiter("\n");
+       
+       System.out.print("\ningrese una frase completa:");
+       String frase=lea.next();
+       
+       System.out.print("\ningrese una longitug minima para las palabras:");
+       int lonMin=lea.nextInt();
+       
+       int contar=0;
+       String palabra="";
+       
+       System.out.println("Palabras con longitud mayor a "+lonMin+":");
+       
+       while(contar<frase.length()){
+           char letra=frase.charAt(contar);
+           
+         if(letra!=' '){
+         palabra+=letra;
+       }else{
+         if(palabra.length()>lonMin){
+         System.out.println("*" + palabra);
+                 }
+         palabra="";
+         }
+         contar++;
+       }
+       
+       
+       
+       } 
 }
-uninin
+      
